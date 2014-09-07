00cfcf0cd4ae503ef9ae4efeb6ecc0a889bb97e8
Noel D'Souza <noel@noeldsouza.com>
2014-09-07 10:32:05 -0400

    Add support for logging.

--- rapt/__init__.py
+++ rapt/__init__.py
@@ -1,6 +1,15 @@
+import logging
+
 from .rapt import Rapt
 
 __version__ = '0.0.1'
+
+# To add logging to a python file insert `log = logging.getLogger(__name__)`
+# near the top at the module level.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# Turn logging on.
+logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
